e167af7232cb1f8c5bdbc8fd5fe53b4aaba25ff5
StigZhang <18237148627@163.com>
2017-12-25 11:53:35 +0800

    checkout flow:payment

--- aistorefront/web/webroot/WEB-INF/_ui-src/responsive/lib/ybase-0.1.0/js/acc.silentorderpost.js
+++ aistorefront/web/webroot/WEB-INF/_ui-src/responsive/lib/ybase-0.1.0/js/acc.silentorderpost.js
@@ -1,7 +1,25 @@
 ACC.silentorderpost = {
 
 	spinner: $("<img src='" + ACC.config.commonResourcePath + "/images/spinner.gif' />"),
-
+	
+	sendRequest:function(callback,dataSend){
+		var options = {
+			    type : 'GET',
+			    url : ACC.config.contextPath + "/auth/getToken",
+			    dataType:"json",
+			    async:false,
+			    error : function(request) {
+			    	console.log('something wrong');
+			    	console.log(request);
+			    },
+			    success : function(result) {
+			    	callback(result,dataSend);
+			    }
+		};
+		
+		$.ajax(options);
+	},
+	
 	bindUseDeliveryAddress: function ()
 	{
 		$('#useDeliveryAddress').on('change', function ()
@@ -29,15 +47,90 @@ ACC.silentorderpost = {
 		}
 	},
 
-	bindSubmitSilentOrderPostForm: function ()
+	bindSubmitSilentOrderPostForm: function (result,dataSend)
 	{
 		$('.submit_silentOrderPostForm').click(function ()
 		{
+			
 			ACC.common.blockFormAndShowProcessingMessage($(this));
 			$('.billingAddressForm').filter(":hidden").remove();
 			ACC.silentorderpost.enableAddressForm();
-			$('#silentOrderPostForm').submit();
+			
+			var data = {};
+			ACC.checkout.sendRequest(ACC.silentorderpost.createPayment,data);
+			//$('#silentOrderPostForm').submit();
 		});
+	},
+	
+	createPayment:function(result,dataSend){
+		var options = {
+				type : 'POST',
+				url : "/cartOptimizationWebservice/v2/"+ACC.config.siteId+"/users/current/carts/"+result.cartUid+"/paymentdetails",
+				
+				headers : {
+					Authorization : "Bearer " + result.token
+				},
+				data : {
+					"accountHolderName" : $("input[name='card_nameOnCard']").val(),
+					"cardNumber" : $("input[name='card_accountNumber']").val(),
+					"cardType" : $("select[name='card_cardType'] option:selected").val(),
+					"expiryMonth" : $("select[name='card_expirationMonth'] option:selected").val(),
+					"expiryYear" : $("select[name='card_expirationYear'] option:selected").val(),
+					//"issueNumber" : $("input[name='card_cvNumber']").val(),
+					
+					//"startMonth" : $("input[name='townCity']").val(),
+					//"startYear" : $("input[name='postcode']").val(),
+					//"subscriptionId" : $("select[name='countryIso'] option:selected").val(),
+					//"defaultPaymentInfo" : $("select[name='regionIso'] option:selected").val(),
+					
+					"saved" : $("input[name='savePaymentInfo']").is(":checked"),
+					
+					//"id" : defaultAddressFlag,
+					"billingAddress.titleCode" : $("select[name='billTo_titleCode'] option:selected").val(),//billTo_titleCode
+					"billingAddress.firstName" : $("input[name='billTo_firstName']").val(),//billTo_firstName
+					"billingAddress.lastName" : $("input[name='billTo_lastName']").val(),
+					"billingAddress.line1" : $("input[name='billTo_street1']").val(),//billTo_street1
+					"billingAddress.line2" : $("input[name='billTo_street2']").val(),//
+					"billingAddress.town" :  $("input[name='billTo_city']").val(),//billTo_city
+					"billingAddress.postalCode" : $("input[name='billTo_postalCode']").val(),//billTo_postalCode
+					"billingAddress.country.isocode" : $("select[name='billTo_country'] option:selected").val(),//billTo_country
+					//"region.isocode" : defaultAddressFlag,
+					//"defaultAddress":
+						
+				},
+				async : false,
+				error : function(request) {
+					console.log("something wrong for addDeliveryAddress");
+				},
+				success : function(data) {
+					console.log(data);
+					window.location = ACC.config.contextPath+'/'+ACC.config.language+'/checkout/multi/summary/view';
+				}
+			};
+			
+			$.ajax(options);
+	},
+	
+	submitSilentOrderPostForm:function(){
+		var options = {
+				type : 'Get',
+				url : "/cartOptimizationWebservice/v2/"+ACC.config.siteId+"/users/current/carts/"+result.cartUid+"/payment/sop/request",
+				
+				headers : {
+					Authorization : "Bearer " + result.token
+				},
+				async : false,
+				error : function(request) {
+					console.log("something wrong for selectDeliveryAddress");
+					console.log(request);
+				},
+				success : function(data) {
+					console.log('data:'+data);
+					window.location = ACC.config.contextPath+'/'+ACC.config.language+'/checkout/multi/payment-method/add';
+				}
+		};
+			
+		$.ajax(options);
 	},
 
 	bindCycleFocusEvent: function ()
